774d1a44d2a051c1a6060047a3913f8003f69496
candra <candraherdiansyah14@gmail.com>
2022-09-08 14:50:55 +0700

    template admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="container-fluid p-0">
+
+        <h1 class="h3 mb-3"><strong>Admin</strong> Dashboard</h1>
+
+        <div class="row">
+
+        </div>
+
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,6 @@
 <?php
 
 use App\Http\Controllers\MyController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,27 +9,20 @@ Route::get('/', function () {
 
 Route::get('about', [MyController::class, 'about']);
 
-// tes model
-Route::get('data-post', function () {
-    $query = \App\Models\Post::all();
-    return $query;
-});
-
-// Route::get('data-faris',function(){
-//     $query = \App\Models\Faridz::all();
-//     return $query;
-// });
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'isAdmin']], function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
     Route::get('profile', function () {
         return view('profile');
     });
+
 });
 
 Route::get('/errors', function () {
-    return view('404');
+    return view('errors.403');
 });
